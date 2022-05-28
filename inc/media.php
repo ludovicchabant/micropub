750a199e02ab0c6388ccf5409efd3858de1b77ca
Ludovic Chabant <ludovic@chabant.com>
2022-05-28 11:29:00 -0700

    Media handling improvements
    - Use Imagick for higher quality resizing and rotating
    - Better handle thumbnail names

--- inc/media.php
+++ inc/media.php
@@ -1,61 +1,104 @@
 <?php
 
-function resize_image($file, $width) {
-    # https://stackoverflow.com/a/25181208/3297734
-    $rotate = 0;
-    $ext = pathinfo(basename($file), PATHINFO_EXTENSION);
-    # if this is a JPEG, read the Exif data in order to
-    # rotate the image, if needed
-    if ($ext == 'jpeg') {
-        $exif = @exif_read_data($file);
-        if (!empty($exif['Orientation'])) {
-            switch ($exif['Orientation']) {
-                case 3:
-                    $rotate = 180;
-                    break;
-                case 6:
-                     $rotate = -90;
-                     break;
-                case 8:
-                     $rotate = 90;
-                     break;
-            }
-        }
+function autorotate_image(Imagick $image) {
+    switch ($image->getImageOrientation()) {
+    case Imagick::ORIENTATION_TOPLEFT:
+        break;
+    case Imagick::ORIENTATION_TOPRIGHT:
+        $image->flopImage();
+        break;
+    case Imagick::ORIENTATION_BOTTOMRIGHT:
+        $image->rotateImage("#000", 180);
+        break;
+    case Imagick::ORIENTATION_BOTTOMLEFT:
+        $image->flopImage();
+        $image->rotateImage("#000", 180);
+        break;
+    case Imagick::ORIENTATION_LEFTTOP:
+        $image->flopImage();
+        $image->rotateImage("#000", -90);
+        break;
+    case Imagick::ORIENTATION_RIGHTTOP:
+        $image->rotateImage("#000", 90);
+        break;
+    case Imagick::ORIENTATION_RIGHTBOTTOM:
+        $image->flopImage();
+        $image->rotateImage("#000", 90);
+        break;
+    case Imagick::ORIENTATION_LEFTBOTTOM:
+        $image->rotateImage("#000", -90);
+        break;
+    default: // Invalid orientation
+        break;
     }
-    $new = @imagecreatefromstring(@file_get_contents($file));
-    if ($rotate !== 0) {
-        $new = imagerotate($new, $rotate, 0);
-    }
-    // resize to our max width
-    $new = imagescale( $new, $width );
-    if ( $new === false ) {
+    $image->setImageOrientation(Imagick::ORIENTATION_TOPLEFT);
+}
+
+function resize_image(Imagick $image, $width, $outpath="") {
+    # handle orientation
+    autorotate_image($image);
+
+    # figure out height based on width
+    $old_width = $image->getImageWidth();
+    $old_height = $image->getImageHeight();
+    if ($old_width <= 0 || $old_height <= 0) {
         quit(415, 'invalid_file', 'Unable to process image.');
     }
-    $width = imagesx($new);
-    $height = imagesy($new);
-    // create a new image from our source, for safety purposes
-    $target = imagecreatetruecolor($width, $height);
-    imagecopy($target, $new, 0, 0, 0, 0, $width, $height);
-    imagedestroy($new);
-    if ( $ext == 'gif' ) {
-        // Convert to palette-based with no dithering and 255 colors
-        imagetruecolortopalette($target, false, 255);
+
+    # do the resize (passing 0 as the height does proportional resizing)
+    $image->resizeImage($width, 0, Imagick::FILTER_TRIANGLE, 1);
+
+    # save file
+    if (!$outpath) {
+        $outpath = $image->getImageFilename();
     }
-    // write the file, using the GD function of the file type
-    $result = call_user_func("image$ext", $target, $file);
-    if ( $result === false ) {
-        quit(400, 'error', 'unable to write image');
+    $result = $image->writeImage($outpath);
+    if ($result !== true) {
+        quit(400, 'error', 'Unable to write image: ' . $outpath);
     }
-    imagedestroy($target);
-    chmod($file, 0644);
+    chmod($outpath, 0644);
+}
+
+function check_upload($file) {
+    global $config;
+
+    # check any php errors
+    if (isset($file['error'])) {
+        switch ($file['error']) {
+        case UPLOAD_ERR_INI_SIZE:
+            quit(413, 'too_large', 'The file exceeds server limits');
+        case UPLOAD_ERR_FORM_SIZE:
+            quit(413, 'too_large', 'The file exceeds the form limits');
+        case UPLOAD_ERR_PARTIAL:
+            quit(400, 'incomplete', 'The file was incompletely uploaded');
+        case UPLOAD_ERR_NO_FILE:
+            quit(400, 'missing', 'No file was uploaded');
+        case UPLOAD_ERR_NO_TMP_DIR:
+            quit(500, 'no_tmp_dir', 'No temp directory exists on the server');
+        case UPLOAD_ERR_CANT_WRITE:
+            quit(500, 'no_write', 'The server has no write access to the temp directory');
+        case UPLOAD_ERR_EXTENSION:
+            quit(500, 'stopped', 'A server module stopped the file upload');
+        }
+    }
+
+    # make sure the file isn't too small
+    if ($file['size'] == 0) {
+        quit(400, 'too_small', 'The file is empty');
+    }
+
+    # make sure the file isn't too large
+    $max_size = isset($config['max_upload_size']) ? $config['max_upload_size'] : 6000000;
+    if ($file['size'] > $max_size)  {
+        quit(413, 'too_large', 'The file is too large.');
+    }
 }
 
 function media_upload($file, $target_dir, $max_width, $thumb_width) {
-    # first make sure the file isn't too large
-    if ( $file['size'] > 6000000 )  {
-        quit(413, 'too_large', 'The file is too large.');
-    }
-    # now make sure it's an image. We only deal with JPG, GIF, PNG right now
+    # make sure there's something valid.
+    check_upload($file);
+    # now make sure it's an image.
+    # we only deal with JPG, GIF, PNG right now.
     $finfo = new finfo(FILEINFO_MIME_TYPE);
     if (false === $ext = array_search($finfo->file($file['tmp_name']),
       array(
@@ -67,22 +110,20 @@ function media_upload($file, $target_dir, $max_width, $thumb_width) {
         quit(415, 'invalid_file', 'Invalid file type was uploaded.');
     }
 
+    $name = pathinfo($file['name'], PATHINFO_FILENAME);
+    # replace spaces with dashes, for sanity and safety
+    $name = str_replace(' ', '-', $name);
     $ext = strtolower(pathinfo(basename($file['name']), PATHINFO_EXTENSION));
-    if ( $ext == 'jpg' ) {
-        # normalize JPEG extension, so we can invoke GD functions easier
-        $ext = 'jpeg';
-    }
-    # define our own name for this file.
-    # and replace spaces with dashes, for sanity and safety
-    $orig = str_replace(' ', '-', explode('.', $file['name'])[0]);
-    $width_suffix = ($max_width > 0 ? '-$max_width' : '') . ".$ext";
-    $filename = $orig . $width_suffix;
-    # if the file already exists, add the date as a suffix
+    $width_suffix = ($max_width > 0 ? '-$max_width' : '');
+    $filename = "$name$width_suffix.$ext";
+
+    # if the file already exists or is a default name, add the date as a suffix
     $date_suffix = '';
-    if (file_exists("$target_dir$filename")) {
+    $is_nondescript = in_array($name, array('image', 'photo', 'file'));
+    if ($is_nondescript || file_exists("$target_dir$filename")) {
         $date = new DateTime();
         $date_suffix = '-' . $date->format('u');
-        $filename = $orig . $date_suffix . $width_suffix;
+        $filename = "$name$date_suffix$width_suffix.$ext";
     }
     if (file_exists("$target_dir$filename")) {
         quit(409, 'file_exists', 'A filename conflict has occurred on the server.');
@@ -93,20 +134,21 @@ function media_upload($file, $target_dir, $max_width, $thumb_width) {
         quit(403, 'file_error', 'Unable to save the uploaded file');
     }
 
-    // check the image and resize if necessary
-    $details = getimagesize("$target_dir$filename");
-    if ( $details === false ) {
-        quit(415, 'invalid_file', 'Invalid file type was uploaded.');
+    // check the image and resize if necessary.
+    try {
+        $image = new Imagick("$target_dir$filename");
+    } catch (Exception $ex) {
+        quit(415, 'invalid_file', 'Invalid file was uploaded.');
     }
-    if ( $max_width > 0 && $details[0] > $max_width ) {
-        resize_image("$target_dir$filename", $max_width );
+    $image_width = $image->getImageWidth();
+    if ($max_width > 0 && $image_width > $max_width ) {
+        resize_image($image, $max_width);
     }
 
-    if ($thumb_width > 0) {
-        # let's make a thumbnail, too.
-        $thumbnail = $orig . $date_suffix . "-$thumb_width.$ext";
-        copy("$target_dir$filename", "$target_dir$thumbnail");
-        resize_image("$target_dir$thumbnail", $thumb_width );
+    # let's make a thumbnail, too.
+    if ($thumb_width > 0 && $image_width > $thumb_width) {
+        $thumbnail = "$name$date_suffix-$thumb_width.$ext";
+        resize_image($image, $thumb_width, "$target_dir$thumbnail");
     } else {
         $thumbnail = false;
     }
